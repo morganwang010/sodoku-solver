92e6c8c6881989014c0495c859130feda96fd321
unknown <morganwang010>
2024-06-18 09:35:21 +0800

    first commit

---
+++ Main.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+from SudokuExtractor import extract_sudoku
+from SudokuSolver import solve
+from NumberExtractor import extract_number, display_sudoku
+import numpy as np
+
+# 读图
+filePath = ".\\imgs\\"
+files = os.listdir(filePath)
+for file in files:
+    path = filePath + file
+
+    image_ori = cv2.imread(path, 0)
+    ratio = image_ori.shape[0] / 540
+    w = round(image_ori.shape[1] / ratio)
+    image_resize = cv2.resize(image_ori, (w, 540))
+
+    image = extract_sudoku(path)
+    image_show = cv2.resize(image, (540, 540))
+
+    result = extract_number(image)
+    display_sudoku(result)
+    result = solve(result)
+    if result is not None:
+        print("Solution is:")
+        display_sudoku(result)
+    else:
+        print("No Solution!")
+
+    image_stack = np.hstack((image_resize, image_show))
+
+    cv2.imshow("image", image_stack)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,12 @@
+# Sudoku-Solving
+##### 1. Environment
+
+- python 3.12     tensorflow 2.26   keras 3.3.3  opencv-python  4.10
+
+##### 2. Usage
+
+* Download all files and run Main.py，your unidentified images are in imgs directory
+
+##### 3. Prerequisites
+
+if you want to train yourself model, first run the CNN_MNIST.py， then run CNN_LOCAL.py，or else the identify accruate is not so good
